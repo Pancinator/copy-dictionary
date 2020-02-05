64f91caa7786db23c125777f57df6cd2874194c0
H390882 <Matej.Pancak@honeywell.com>
2020-02-05 13:54:56 +0100

    New move function updated

--- app-copy-directory.py
+++ app-copy-directory.py
@@ -1,6 +1,8 @@
 import shutil
 import argparse
 import os
+import shutil
+import hashlib, os
 
 parser = argparse.ArgumentParser(description='Manipulating with file structures.')
 parser.add_argument('-c', '--copy', dest='copy', nargs='+',
@@ -14,7 +16,12 @@ parser.add_argument('-m', '--move', dest='move', nargs='+',
                          'The source folder will be moved to new destination folder, origin source folder will be deleted'
                          'If the destination folder does not exist, it will be created.')
 
+parser.add_argument('-s', '--sha', dest='sha', nargs='+',
+                    help='[path to folder]'
+                         'Create and md5 hash of the selected file or folder')
+
 args = parser.parse_args()
+directory = args.sha[0]
 #Copy argument usage
 def copy_directory():
     try:
@@ -24,7 +31,7 @@ def copy_directory():
 
         with(open(new_folder_path+'\README.txt', 'w')) as readme:
                 readme.write(f'this Folders structure was copied from {args.copy[0]}'
-                             f'The copy action was processed by copy-directory.py from pancakefactory.com'))
+                             f'The copy action was processed by copy-directory.py from pancakefactory.com')
     except:
         print('Some problem occurs, please be sure that the paths of directories are correct')
 
@@ -42,8 +49,31 @@ def move_directory():
     except:
         print('Some problem occurs, please be sure that the paths of directories are correct')
 
+def craete_md5(directory, verbose=0):
+
+    if not os.path.exists(directory):
+        print('Directory or file does not exist')
+    try:
+        for root, dirs, files in os.walk(directory):
+            for names in files:
+                if verbose == 1:
+                    print('Hashing', names)
+            filepath = os.path.join(root,names)
+            with(open(filepath, 'rb')) as file:
+                file_content = file.read()
+                SHAhash = hashlib.md5(file_content)
+                print(SHAhash.hexdigest())
+    except:
+        import traceback
+    # Print the stack traceback
+        traceback.print_exc()
+        return -2
+
+    return SHAhash.hexdigest()
 
 if args.copy != [] and args.copy != None:
     copy_directory()
-elif args.move != []:
+elif args.move != [] and args.move != None:
     move_directory()
+elif args.sha !=  [] and args.sha != None:
+    print(craete_md5(directory, 1))
